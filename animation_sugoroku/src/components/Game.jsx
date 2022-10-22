d783eed25ffdb86c245fe70d6612c63b4e0c3632
Raystar247 <24star10@gmail.com>
2022-10-23 01:22:08 +0900

    extract GoalModal.jsx from EventModal Goal用のModalを分離(未編集)

--- animation_sugoroku/src/components/Game.jsx
+++ animation_sugoroku/src/components/Game.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import Dice2 from './Dice2';
 import Board from "./Board";
 import EventModal from "./EventModal";
+import GoalModal from "./GoalModal";
 import FinishModal from "./FinishModal";
 import PlayerList from './PlayerList';
 
@@ -54,6 +55,7 @@ export default class Game extends React.Component {
         //state
         this.state = this.getState();//すごろくゲームに関するstateを取得
         this.state["isEventModalVisible"] = false;//イベント処理時のモーダルの表示フラグのstate
+        this.state["isGoalModalVisible"] = false;
         this.state["isFinishModalVisible"] = false;
         this.state["onModalClosedMethod"] = null;//モーダルを閉じたときの処理
         this.state["modalContent"] = null;//モーダルの中身
@@ -158,7 +160,7 @@ export default class Game extends React.Component {
                 });
                 this.setFinishModalClosedMethod(newState.playerList);//モーダルを閉じるときの処理をセット
                 audio_success.play();
-                setTimeout(() => this.setState({ isEventModalVisible: true }), 500);//モーダルを表示
+                setTimeout(() => this.setState({ isGoalModalVisible: true }), 500);//モーダルを表示
             }
             else {//ゴールでない場合，サイコロを有効にして次の人に番が回る
                 setTimeout(() => this.diceRef.current.switchDiceButtonDisabled(false), 500);//サイコロボタンを有効にする．setStateが非同期なため，少し遅延を入れている
@@ -271,6 +273,11 @@ export default class Game extends React.Component {
                     {/* イベントやマスクリックで出てくるモーダル．サイドメニューより前面に出すためにDrawerの子にしている */}
                     <EventModal
                         isOpen={this.state.isEventModalVisible}
+                        modalContent={this.state.modalContent}
+                        onClose={this.state.onModalClosedMethod}
+                    />
+                    <GoalModal
+                        isOpen={this.state.isGoalModalVisible}
                         modalContent={this.state.modalContent}
                         onClose={this.state.onModalClosedMethod}
                     />
